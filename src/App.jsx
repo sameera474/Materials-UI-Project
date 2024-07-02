ff4c6aafa6f361df0882398a0584b851b3de7295
Sameera Chaturanga Wagaarachchige <sameera474@gmail.com>
2024-07-02 06:19:23 +0300

    Add some  Typography variants

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { MuiTypography } from "./components/MuiTypography";
 function App() {
-  return <></>;
+  return (
+    <>
+      <MuiTypography />
+    </>
+  );
 }
 
 export default App;
--- src/components/MuiTypography.jsx
+++ src/components/MuiTypography.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
 
 export const MuiTypography = () => {
-  return <div>MuiTypography</div>;
+  return (
+    <div>
+      <Typography variant="h1">Heading 1</Typography>
+      <Typography variant="h2">Heading 2</Typography>
+      <Typography variant="h3">Heading 3</Typography>
+      <Typography variant="h4">Heading 4</Typography>
+      <Typography variant="subtitle1" gutterBottom>
+        subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore
+        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+        fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+    </div>
+  );
 };
